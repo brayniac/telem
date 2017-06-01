194f134bc1897d367cf120f4b166a2dea6580d6e
Brian Martin <brayniac@gmail.com>
2017-06-01 08:37:02 -0700

    refactor (#4)
    - simplify main
    - more structs

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,10 +1,10 @@
-use perfcnt::PerfCounter;
-use perfcnt::linux::{HardwareEventType, PerfCounterBuilderLinux, SoftwareEventType};
-use std::collections::HashMap;
+use perfcnt::AbstractPerfCounter;
+use shuteye;
+use sources::PerfCounters;
 use std::fmt;
+use std::thread;
 use std::time::Duration;
-use tic::{Interest, Receiver};
-use x86;
+use tic::{Clocksource, Interest, Receiver, Sample, Sender};
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub enum Metric {
@@ -47,68 +47,62 @@ impl fmt::Display for Metric {
     }
 }
 
-pub fn get_counters() -> HashMap<Metric, PerfCounter> {
-    let mut h = HashMap::new();
-    for (key, descr) in x86::perfcnt::core_counters().unwrap() {
-        if let Some(metric) = match *key {
-               "MEM_LOAD_UOPS_RETIRED.L1_HIT" => Some(Metric::MemoryLoadsL1Hit),
-               "MEM_LOAD_UOPS_RETIRED.L1_MISS" => Some(Metric::MemoryLoadsL1Miss),
-               "MEM_LOAD_UOPS_RETIRED.L2_HIT" => Some(Metric::MemoryLoadsL2Hit),
-               "MEM_LOAD_UOPS_RETIRED.L2_MISS" => Some(Metric::MemoryLoadsL2Miss),
-               "MEM_LOAD_UOPS_RETIRED.L3_HIT" => Some(Metric::MemoryLoadsL3Hit),
-               "MEM_LOAD_UOPS_RETIRED.L3_MISS" => Some(Metric::MemoryLoadsL3Miss),
-               "MEM_UOPS_RETIRED.ALL_LOADS" => Some(Metric::MemoryLoads),
-               "MEM_UOPS_RETIRED.ALL_STORES" => Some(Metric::MemoryStores),
-               "MEM_UOPS_RETIRED.SPLIT_LOADS" => Some(Metric::MemoryLoadsSplit),
-               "MEM_UOPS_RETIRED.SPLIT_STORES" => Some(Metric::MemoryStoresSplit),
-               "UOPS_RETIRED.ALL" => Some(Metric::UopsRetired),
-               _ => None,
-           } {
-            h.insert(metric,
-                     PerfCounterBuilderLinux::from_intel_event_description(descr)
-                         .finish()
-                         .unwrap());
+pub struct Collector {
+    perf: PerfCounters,
+    sender: Sender<Metric>,
+    clock: Clocksource,
+}
+
+impl Collector {
+    pub fn new(listen: Option<String>) -> Collector {
+        let mut config = Receiver::<Metric>::configure()
+            .batch_size(1024)
+            .duration(1)
+            .capacity(4096)
+            .poll_delay(Some(Duration::new(0, 1_000_000)))
+            .service(true);
+
+        if let Some(addr) = listen {
+            config = config.http_listen(addr);
+        }
+
+        let mut receiver = config.build();
+
+        let perf = PerfCounters::new();
+
+        for metric in perf.get_metrics() {
+            receiver.add_interest(Interest::Count(metric));
+        }
+
+        let sender = receiver.get_sender();
+        let clocksource = receiver.get_clocksource();
+
+        thread::spawn(move || receiver.run());
+
+        Collector {
+            perf: perf,
+            sender: sender,
+            clock: clocksource,
         }
     }
 
-    h.insert(Metric::Instructions,
-             PerfCounterBuilderLinux::from_hardware_event(HardwareEventType::Instructions)
-                 .finish()
-                 .unwrap());
-    h.insert(Metric::Cycles,
-             PerfCounterBuilderLinux::from_hardware_event(HardwareEventType::RefCPUCycles)
-                 .finish()
-                 .unwrap());
-
-    h.insert(Metric::PageFaults,
-             PerfCounterBuilderLinux::from_software_event(SoftwareEventType::PageFaults)
-                 .finish()
-                 .unwrap());
-    h.insert(Metric::ContextSwitches,
-             PerfCounterBuilderLinux::from_software_event(SoftwareEventType::ContextSwitches)
-                 .finish()
-                 .unwrap());
-
-    h
-}
-
-pub fn init(listen: Option<String>) -> Receiver<Metric> {
-    let mut config = Receiver::<Metric>::configure()
-        .batch_size(1024)
-        .duration(1)
-        .capacity(4096)
-        .poll_delay(Some(Duration::new(0, 1_000_000)))
-        .service(true);
-
-    if let Some(addr) = listen {
-        config = config.http_listen(addr);
+    pub fn collect(&mut self) {
+        let t0 = self.clock.counter();
+        for counter in self.perf.counters.values_mut() {
+            counter.start().expect("Could not start perf counter");
+        }
+        shuteye::sleep(Duration::new(0, 1_000_000));
+        let t1 = self.clock.counter();
+        for counter in self.perf.counters.values_mut() {
+            counter.stop().expect("Could not start perf counter");
+        }
+        for (metric, counter) in &mut self.perf.counters {
+            let _ = self.sender
+                .send(Sample::counted(t0,
+                                      t1,
+                                      counter.read().expect("Could not read perf counter"),
+                                      metric.clone()));
+            counter.reset().expect("Could not reset perf counter");
+        }
     }
-
-    let mut receiver = config.build();
-
-    for (metric, _) in get_counters() {
-        receiver.add_interest(Interest::Count(metric));
-    }
-
-    receiver
 }
