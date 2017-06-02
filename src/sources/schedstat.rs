61432bdfb3ffa80e9b9c4cadcaf99fb64b5afadb
Brian Martin <brayniac@gmail.com>
2017-06-02 08:43:56 -0700

    rustfmt

--- src/sources/schedstat.rs
+++ src/sources/schedstat.rs
@@ -1,58 +1,59 @@
 use metrics::Metric;
 use std::collections::HashMap;
-use tic::{Clocksource, Sample, Sender};
+use std::fs::File;
 use std::io::{self, BufReader};
 use std::io::prelude::*;
-use std::fs::File;
+use tic::{Clocksource, Sample, Sender};
 
 #[derive(Clone)]
 pub struct Counters {
-	time: u64,
-	data: HashMap<Metric, u64>,
+    time: u64,
+    data: HashMap<Metric, u64>,
 }
 
 impl Counters {
-	pub fn new(time: u64) -> Counters {
-		let mut data = HashMap::new();
+    pub fn new(time: u64) -> Counters {
+        let mut data = HashMap::new();
 
-		let f = File::open("/proc/schedstat").unwrap();
-		let f = BufReader::new(f);
+        let f = File::open("/proc/schedstat").unwrap();
+        let f = BufReader::new(f);
 
-		for line in f.lines() {
-			let line = line.unwrap();
-			let tokens: Vec<&str> = line.split_whitespace().collect();
-			if tokens.len() != 10 {
-				continue;
-			}
-			let label = tokens[0];
-			let value = tokens[7];
-			data.insert(Metric::CpuRunningMilliseconds(label.to_owned()), value.parse().unwrap_or(0));
+        for line in f.lines() {
+            let line = line.unwrap();
+            let tokens: Vec<&str> = line.split_whitespace().collect();
+            if tokens.len() != 10 {
+                continue;
+            }
+            let label = tokens[0];
+            let value = tokens[7];
+            data.insert(Metric::CpuRunningMilliseconds(label.to_owned()),
+                        value.parse().unwrap_or(0));
 
-		    //println!("{}", line.unwrap());
-		}
+            //println!("{}", line.unwrap());
+        }
 
-		Counters {
-			time: time,
-			data: data,
-		}
-	}
+        Counters {
+            time: time,
+            data: data,
+        }
+    }
 
-	pub fn metrics(&self) -> Vec<Metric> {
-		let mut v = Vec::new();
-		for metric in self.data.keys() {
-			v.push(metric.clone());
-		}
-		v
-	}
+    pub fn metrics(&self) -> Vec<Metric> {
+        let mut v = Vec::new();
+        for metric in self.data.keys() {
+            v.push(metric.clone());
+        }
+        v
+    }
 
-	pub fn sample(&mut self, sender: &mut Sender<Metric>, clock: &Clocksource) {
-		let new = Counters::new(clock.counter());
-		for (metric, v1) in new.clone().data {
-			if let Some(v0) = self.data.get(&metric) {
-				let _ = sender.send(Sample::counted(self.time, new.time, v1 - v0, metric));
-			}
-		}
-		self.data = new.data;
-		self.time = new.time;
-	}
+    pub fn sample(&mut self, sender: &mut Sender<Metric>, clock: &Clocksource) {
+        let new = Counters::new(clock.counter());
+        for (metric, v1) in new.clone().data {
+            if let Some(v0) = self.data.get(&metric) {
+                let _ = sender.send(Sample::counted(self.time, new.time, v1 - v0, metric));
+            }
+        }
+        self.data = new.data;
+        self.time = new.time;
+    }
 }
